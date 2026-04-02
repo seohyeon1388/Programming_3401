7774048f21a1a1e63c1ce97b893054e8b9599f76
seohyeon1388 <seohyeongang010@gmail.com>
2026-04-02 09:53:23 +0900

    CounterApp +

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,16 +5,18 @@ import heroImg from "./assets/hero.png";
 // import "./App.css";
 
 function CounterApp() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //tkdxorkqtm dlwjs tkdxorkqt; setCount() : 상태를 바꾸는 함수. 반드시 얘로 바꿔야함
 
   return (
     <>
-      <button
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}
       >
         Count is {count}
-      </button>
+      </button> */}
     </>
   );
 }
