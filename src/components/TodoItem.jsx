e10a54a590fe8e2ccc714372cc27e578548a335e
seohyeon1388 <seohyeongang010@gmail.com>
2026-04-23 08:53:52 +0900

    TodoListApp show todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,10 @@
 import Checkbox from "./Checkbox.jsx";
 import Button from "./Button.jsx";
 
-export default function TodoItem() {
+export default function TodoItem({ todo }) {
   return (
-    <li className="todo__item todo__item--complete">
-      <Checkbox id="1">지각 하기</Checkbox>
+    <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
+      <Checkbox id="{todo.id}">{todo.text}</Checkbox>
       <Button className="todo__button todo__button--edit">✏️</Button>
       <Button className="todo__button todo__button--delete">❌</Button>
     </li>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,13 @@
 import TodoItemEmpty from "./TodoItemEmpty.jsx";
 import TodoItem from "./TodoItem.jsx";
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
     <>
       <ul className="todo__list">
-        <TodoItemEmpty />
-        <TodoItem />
+        {/* 아무것도 없을 때 */}
+        {todos.length === 0 && <TodoItemEmpty />}
+        {/* 뭐가 잇을 때 */}
+        {todos.length > 0 && todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
       </ul>
     </>
   );
